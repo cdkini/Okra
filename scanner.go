0ba59ae82765d7421d612938380bca820d5a30ca
Chetan Kini <ckini123@gmail.com>
2020-05-22 18:38:55 -0400

    Added scanTokens()

--- scanner.go
+++ scanner.go
@@ -1,1 +1,27 @@
 package main
+
+type Scanner struct {
+	source   string
+	tokens   []Token
+	start    int
+	current  int
+	line     int
+	hasError bool
+}
+
+func NewScanner(source string) *Scanner {
+	return &Scanner{source, make([]Token, 0), 0, 0, 0, false}
+}
+
+func (s *Scanner) scanTokens() ([]Token, error) {
+	for s.current < len(s.source) {
+		s.start = s.current
+		s.scan()
+	}
+
+	s.tokens = append(s.tokens, Token{EOF, "", nil, s.line})
+}
+
+func (s *Scanner) scan() {
+
+}
