31a9fe179ae802542b7633c8edc7b8c8e6a823cd
Chetan Kini <ckini123@gmail.com>
2020-05-22 17:15:13 -0400

    Added enum

--- token.go
+++ token.go
@@ -1,5 +1,46 @@
 package main
 
-type Token int
+type TokenType int
 
-const ()
+const (
+	LeftParen TokenType = iota
+	RightParen
+	LeftBrace
+	RightBrace
+	LeftBracket
+	RightBracket
+	Comma
+	Dot
+	Minus
+	Plus
+	Semicolon
+	Slash
+	Start
+	Bang
+	BangEqual
+	Equal
+	EqualEqual
+	Greater
+	GreaterEqual
+	Less
+	LessEqual
+	Identifier
+	String
+	Numeric
+	And
+	Class
+	Else
+	False
+	Func
+	For
+	If
+	Null
+	Or
+	Log
+	Return
+	Super
+	This
+	True
+	Variable
+	EndOfFile
+)
