f5747fae2e7c8fa40cea88226209acc60e49d1ff
Chetan Kini <ckini123@gmail.com>
2020-06-02 18:30:36 -0400

    Added method signatures

--- okra/interpret/parser.go
+++ okra/interpret/parser.go
@@ -5,6 +5,30 @@ struct Parser {
 	curr int
 }
 
-func evaluate() Expr {
+func (p *Parser) evaluate() Expr {
+	return equality()
+}
+
+func (p *Parser) equality() Expr {
+
+}
+
+func (p *Parser) comparsion() Expr {
+
+}
+
+func (p *Parser) addOrSubtract() Expr {
+
+}
+
+func (p *Parser) multiplyOrDivide() Expr {
+
+}
+
+func (p *Parser) unary() Expr {
+
+}
+
+func (p *Parser) primary() Expr {
 	
 }
