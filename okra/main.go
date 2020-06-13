76c06f88a9fd569f2b931f3756590c0d72528abe
Chetan Kini <ckini123@gmail.com>
2020-06-13 12:47:49 -0400

    Removed punctuation from errors

--- okra/main.go
+++ okra/main.go
@@ -11,14 +11,14 @@ import (
 
 func main() {
 	if len(os.Args) != 2 || !strings.HasSuffix(os.Args[1], ".okr") {
-		interpret.ThrowErr(-1, interpret.NewOkraError(0, 0, "Must use \"okra [script]\" to run a .okr file."))
+		interpret.ThrowErr(-1, interpret.NewOkraError(0, 0, "Must use \"okra [script]\" to run a .okr file"))
 	}
 	runFile(os.Args[1])
 }
 
 func runFile(path string) {
 	bytes, err := ioutil.ReadFile(path)
-	interpret.CheckErr(-1, err, interpret.NewOkraError(0, 0, "Path not found."))
+	interpret.CheckErr(-1, err, interpret.NewOkraError(0, 0, "Path not found"))
 	scanner := interpret.NewScanner(string(bytes))
 	tokens := scanner.ScanTokens()
 	parser := interpret.NewParser(tokens)
