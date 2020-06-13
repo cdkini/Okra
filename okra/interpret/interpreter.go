2b148f1a6ce102fd5d84f0d0263e93456a53f919
Chetan Kini <ckini123@gmail.com>
2020-06-13 12:24:55 -0400

    Added interpret methods for exprs

--- okra/interpret/interpreter.go
+++ okra/interpret/interpreter.go
@@ -1,4 +1,25 @@
 package interpret
 
 type Interpreter struct {
+	ExprVisitor
+}
+
+func (i *Interpreter) interpretLiteral(l Literal) interface{} {
+	return l.val
+}
+
+func (i *Interpreter) interpretGrouping(g Grouping) interface{} {
+	return i.evaluate(g.expression)
+}
+
+func (i *Interpreter) interpretUnary(u Unary) interface{} {
+	return nil // TODO: Implement!
+}
+
+func (i *Interpreter) interpretBinary(b Binary) interface{} {
+	return nil // TODO: Implement!
+}
+
+func (i *Interpreter) evaluate(e Expr) interface{} {
+	return e.accept(e)
 }
