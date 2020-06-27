8cda50b7d76a123cc1b874887fa2ed0196790ce3
Chetan Kini <ckini123@gmail.com>
2020-06-27 12:51:30 -0400

    Added String() methods to all Expr classes

--- okra/interpret/expr.go
+++ okra/interpret/expr.go
@@ -1,10 +1,16 @@
 package interpret
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Expr interface {
-	accept(ExprVisitor) error
+	accept(Visitor) error
+	String() string
 }
 
-type ExprVisitor interface {
+type Visitor interface {
 	visitUnary(Unary) error
 	visitBinary(Binary) error
 	visitGrouping(Grouping) error
@@ -16,8 +22,18 @@ type Unary struct {
 	operand  Expr
 }
 
-func (u Unary) accept(ev ExprVisitor) error {
-	return ev.visitUnary(u)
+func (u Unary) String() string {
+	var sb strings.Builder
+	sb.WriteString("(")
+	sb.WriteString(u.operator.lexeme)
+	sb.WriteString(" ")
+	sb.WriteString(u.operand.String())
+	sb.WriteString(")")
+	return sb.String()
+}
+
+func (u Unary) accept(v Visitor) error {
+	return v.visitUnary(u)
 }
 
 type Binary struct {
@@ -26,22 +42,46 @@ type Binary struct {
 	rightOperand Expr
 }
 
-func (b Binary) accept(ev ExprVisitor) error {
-	return ev.visitBinary(b)
+func (b Binary) String() string {
+	var sb strings.Builder
+	sb.WriteString("(")
+	sb.WriteString(b.operator.lexeme)
+	sb.WriteString(" ")
+	sb.WriteString(b.leftOperand.String())
+	sb.WriteString(" ")
+	sb.WriteString(b.rightOperand.String())
+	sb.WriteString(")")
+	return sb.String()
+}
+
+func (b Binary) accept(v Visitor) error {
+	return v.visitBinary(b)
 }
 
 type Grouping struct {
 	expression Expr
 }
 
-func (g Grouping) accept(ev ExprVisitor) error {
-	return ev.visitGrouping(g)
+func (g Grouping) String() string {
+	var sb strings.Builder
+	sb.WriteString("(")
+	sb.WriteString(g.expression.String())
+	sb.WriteString(")")
+	return sb.String()
+}
+
+func (g Grouping) accept(v Visitor) error {
+	return v.visitGrouping(g)
 }
 
 type Literal struct {
 	val interface{}
 }
 
-func (l Literal) accept(ev ExprVisitor) error {
-	return ev.visitLiteral(l)
+func (l Literal) String() string {
+	return fmt.Sprintf("%v", l.val)
+}
+
+func (l Literal) accept(v Visitor) error {
+	return v.visitLiteral(l)
 }
