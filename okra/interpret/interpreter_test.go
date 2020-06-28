ea3886663f7e3a756e356b1e69b5550e030aca1f
Chetan Kini <ckini123@gmail.com>
2020-06-28 10:34:12 -0400

    Added test table for interpreter

--- okra/interpret/interpreter_test.go
+++ okra/interpret/interpreter_test.go
@@ -1,1 +1,28 @@
 package interpret
+
+import "testing"
+
+func TestInterpreting(t *testing.T) {
+	table := []struct {
+		input  string
+		output string
+	}{
+		// TODO: Fill in interpreter tests!"}
+		{"", ""},
+	}
+
+	for _, test := range table {
+		t.Run(test.input, func(t *testing.T) {
+			scanner := NewScanner(test.input)
+			scanner.ScanTokens()
+			parser := NewParser(scanner.tokens)
+			expr := parser.Parse()
+			interpreter := NewInterpreter()
+			res := interpreter.Interpret(expr)
+
+			if res != test.output {
+				t.Errorf("Expected %q, received %q", test.output, res)
+			}
+		})
+	}
+}
