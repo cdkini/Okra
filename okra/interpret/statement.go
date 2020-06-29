a38f451989648512a631afa892188ae2b6a6af84
Chetan Kini <ckini123@gmail.com>
2020-06-29 13:34:24 -0400

    Removed interface{} return values from all Stmts

--- okra/interpret/statement.go
+++ okra/interpret/statement.go
@@ -3,18 +3,18 @@ package interpret
 // TODO: Add docstring
 // TODO: Explain Visitor design pattern better!
 type Stmt interface {
-	accept(StmtVisitor) interface{} // TODO: Explain Visitor design pattern
+	accept(StmtVisitor) // TODO: Explain Visitor design pattern
 }
 
 // TODO: Explain Visitor design pattern
 type StmtVisitor interface {
-	visitExpressionStmt(Expression) interface{}
+	visitExpressionStmt(Expression)
 }
 
 type Expression struct {
 	expr Expr
 }
 
-func (e Expression) accept(v StmtVisitor) interface{} {
-	return v.visitExpressionStmt(e)
+func (e Expression) accept(v StmtVisitor) {
+	v.visitExpressionStmt(e)
 }
