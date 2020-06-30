af6e68a16707595624cf08d0ec8ff7c8d7a408ef
Chetan Kini <ckini123@gmail.com>
2020-06-30 19:47:58 -0400

    Renamed receivers and parameters

--- okra/interpret/expression.go
+++ okra/interpret/expression.go
@@ -29,14 +29,14 @@ type UnaryExpr struct {
 	operand  Expr
 }
 
-func (ue UnaryExpr) String() string {
+func (u UnaryExpr) String() string {
 	var sb strings.Builder
-	sb.WriteString("(" + ue.operator.lexeme + " " + ue.operand.String() + ")")
+	sb.WriteString("(" + u.operator.lexeme + " " + u.operand.String() + ")")
 	return sb.String()
 }
 
-func (ue UnaryExpr) accept(ev ExprVisitor) interface{} {
-	return ev.visitUnaryExpr(ue)
+func (u UnaryExpr) accept(visitor ExprVisitor) interface{} {
+	return visitor.visitUnaryExpr(u)
 }
 
 // A BinaryExpr expression is one that applies a single operator to a multiple operands.
@@ -47,14 +47,14 @@ type BinaryExpr struct {
 	rightOperand Expr
 }
 
-func (be BinaryExpr) String() string {
+func (b BinaryExpr) String() string {
 	var sb strings.Builder
-	sb.WriteString("(" + be.operator.lexeme + " " + be.leftOperand.String() + " " + be.rightOperand.String() + ")")
+	sb.WriteString("(" + b.operator.lexeme + " " + b.leftOperand.String() + " " + b.rightOperand.String() + ")")
 	return sb.String()
 }
 
-func (be BinaryExpr) accept(ve ExprVisitor) interface{} {
-	return ve.visitBinaryExpr(be)
+func (b BinaryExpr) accept(visitor ExprVisitor) interface{} {
+	return visitor.visitBinaryExpr(b)
 }
 
 // A GroupingExpr sets a higher level of precedence for another expression within its bounds.
@@ -63,14 +63,14 @@ type GroupingExpr struct {
 	expression Expr
 }
 
-func (ge GroupingExpr) String() string {
+func (g GroupingExpr) String() string {
 	var sb strings.Builder
-	sb.WriteString("(" + ge.expression.String() + ")")
+	sb.WriteString("(" + g.expression.String() + ")")
 	return sb.String()
 }
 
-func (ge GroupingExpr) accept(ve ExprVisitor) interface{} {
-	return ve.visitGroupingExpr(ge)
+func (g GroupingExpr) accept(visitor ExprVisitor) interface{} {
+	return visitor.visitGroupingExpr(g)
 }
 
 // A LiteralExpr is the most basic expression type and represents a fully evaluated value.
@@ -79,22 +79,22 @@ type LiteralExpr struct {
 	val interface{}
 }
 
-func (le LiteralExpr) String() string {
-	return fmt.Sprintf("%v", le.val)
+func (l LiteralExpr) String() string {
+	return fmt.Sprintf("%v", l.val)
 }
 
-func (le LiteralExpr) accept(ev ExprVisitor) interface{} {
-	return ev.visitLiteralExpr(le)
+func (l LiteralExpr) accept(visitor ExprVisitor) interface{} {
+	return visitor.visitLiteralExpr(l)
 }
 
 type VariableExpr struct {
 	identifier Token
 }
 
-func (ve VariableExpr) String() string {
-	return ve.identifier.lexeme
+func (v VariableExpr) String() string {
+	return v.identifier.lexeme
 }
 
-func (ve VariableExpr) accept(ev ExprVisitor) interface{} {
-	return ev.visitVariableExpr(ve)
+func (v VariableExpr) accept(visitor ExprVisitor) interface{} {
+	return visitor.visitVariableExpr(v)
 }
