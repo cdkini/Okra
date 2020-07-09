0d2b1e46993e5f9aafd07f13bbe6bed03499fe00
Chetan Kini <ckini123@gmail.com>
2020-07-09 18:28:46 -0400

    Removed prior structure dependencies from tests

--- okra/interpret/interpreter_test.go
+++ okra/interpret/interpreter_test.go
@@ -3,27 +3,7 @@ package interpret
 import "testing"
 
 func TestInterpreting(t *testing.T) {
-	table := []struct {
-		input  string
-		output string
-	}{
-		// TODO: Fill in interpreter tests!"}
-		{"-1", ""},
-	}
-
-	for _, test := range table {
-		t.Run(test.input, func(t *testing.T) {
-			// FIXME: Test broken after addition of statements
-			scanner := NewScanner(test.input)
-			scanner.ScanTokens()
-			parser := NewParser(scanner.tokens)
-			stmts := parser.Parse()
-			interpreter := NewInterpreter()
-			interpreter.Interpret(stmts)
-
-			// if res != test.output {
-			// 	t.Errorf("Expected %q, received %q", test.output, res)
-			// }
-		})
-	}
+	// TODO: Add tests
+	// Input: Stmt[]
+	// Output:
 }
--- okra/interpret/parser_test.go
+++ okra/interpret/parser_test.go
@@ -5,27 +5,7 @@ import (
 )
 
 func TestParsing(t *testing.T) {
-	table := []struct {
-		input  string
-		output string
-	}{
-		// TODO: Fill in parser tests!"}
-		{"1 + 2", "(+ 1 2)"},
-		{"1 * 2 + 1 / 3", "()"},
-	}
-
-	for _, test := range table {
-		t.Run(test.input, func(t *testing.T) {
-			// FIXME: Test broken after addition of statements
-			// scanner := NewScanner(test.input)
-			// scanner.ScanTokens()
-			// parser := NewParser(scanner.tokens)
-			// expr := parser.Parse()
-			// str := expr.String()
-
-			// if str != test.output {
-			// 	t.Errorf("Expected %q, received %q", test.output, expr)
-			// }
-		})
-	}
+	// TODO: Add tests
+	// Input: tokens []*Token
+	// Output: string ???
 }
