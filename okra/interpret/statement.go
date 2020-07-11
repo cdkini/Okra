0b06191b4b51d69ea49b7e5fbf4dfdef8d51d6c1
Chetan Kini <ckini123@gmail.com>
2020-07-10 20:21:11 -0400

    Added String() and getType() to Stmt

--- okra/interpret/statement.go
+++ okra/interpret/statement.go
@@ -1,8 +1,14 @@
 package interpret
 
+import (
+	"reflect"
+)
+
 // TODO: Explain Visitor design pattern better!
 type Stmt interface {
 	accept(StmtVisitor) // TODO: Explain Visitor design pattern
+	String() string
+	getType() string
 }
 
 // TODO: Explain Visitor design pattern
@@ -21,6 +27,14 @@ func (e ExpressionStmt) accept(vst StmtVisitor) {
 	vst.visitExpressionStmt(e)
 }
 
+func (e ExpressionStmt) String() string {
+	return e.getType() + ": " + e.expr.String()
+}
+
+func (e ExpressionStmt) getType() string {
+	return reflect.TypeOf(e).String()
+}
+
 // TODO: Add docstring
 type PrintStmt struct {
 	expr Expr
@@ -28,6 +42,14 @@ type PrintStmt struct {
 
 func (p PrintStmt) accept(vst StmtVisitor) {
 	vst.visitPrintStmt(p)
+}
+
+func (p PrintStmt) String() string {
+	return p.getType() + p.getType() + ": " + p.expr.String()
+}
+
+func (p PrintStmt) getType() string {
+	return reflect.TypeOf(p).String()
 }
 
 // TODO: Add docstring
@@ -38,4 +60,12 @@ type VariableStmt struct {
 
 func (v VariableStmt) accept(vst StmtVisitor) {
 	vst.visitVariableStmt(v)
+}
+
+func (v VariableStmt) String() string {
+	return v.getType() + v.getType() + ": " + v.identifier.String() + " = " + v.expr.String()
+}
+
+func (v VariableStmt) getType() string {
+	return reflect.TypeOf(v).String()
 }
