9074c52e186eb73bf6df3f00bd35a8005b1a49ed
Chetan Kini <ckini123@gmail.com>
2020-08-28 20:19:38 -0400

    Renamed key-value mapping to vals

--- okra/interpreter/interpret/environment.go
+++ okra/interpreter/interpret/environment.go
@@ -8,20 +8,20 @@ import (
 // TODO: Add docstring
 type Environment struct {
 	enclosing *Environment
-	vars      map[string]interface{}
+	vals      map[string]interface{}
 }
 
 func NewEnvironment(enclosing *Environment) *Environment {
-	return &Environment{enclosing, make(map[string]interface{})}
+	return &Environment{enclosing, make(map[string]interface{}, 0)}
 }
 
 func (e *Environment) Define(identifier string, value interface{}) {
-	e.vars[identifier] = value
+	e.vals[identifier] = value
 }
 
 func (e *Environment) Assign(token ast.Token, value interface{}) {
-	if _, ok := e.vars[token.Lexeme]; ok {
-		e.vars[token.Lexeme] = value
+	if _, ok := e.vals[token.Lexeme]; ok {
+		e.vals[token.Lexeme] = value
 	} else if e.enclosing != nil {
 		e.enclosing.Assign(token, value)
 	} else {
@@ -30,7 +30,7 @@ func (e *Environment) Assign(token ast.Token, value interface{}) {
 }
 
 func (e *Environment) Get(token ast.Token) interface{} {
-	if val, ok := e.vars[token.Lexeme]; ok {
+	if val, ok := e.vals[token.Lexeme]; ok {
 		return val
 	}
 	if e.enclosing != nil {
