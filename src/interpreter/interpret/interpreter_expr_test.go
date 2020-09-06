3848f5d93d5644fed1e1f7819a9b290cb0a517ae
Chetan Kini <ckini123@gmail.com>
2020-09-06 14:51:09 -0400

    Added test tables to interpreter unit test files

--- src/interpreter/interpret/interpreter_expr_test.go
+++ src/interpreter/interpret/interpreter_expr_test.go
@@ -1,47 +1,226 @@
 package interpret
 
-import "testing"
+import (
+	. "Okra/src/interpreter/ast"
+	"testing"
+)
 
 func TestInterpretAssignmentExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretAssignmentExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretBinaryExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretBinaryExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretCallExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretCallExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretGetExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretGetExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretGroupingExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretGroupingExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretLiteralExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretLiteralExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretLogicalExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretLogicalExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretSetExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretSetExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretThisExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretThisExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretUnaryExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretUnaryExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
 
 func TestInterpretVariableExpr(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Expr
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretVariableExpr", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			expr := interpreter.interpretExpr(test.input)
+
+			if expr != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, expr)
+			}
+		})
+	}
 }
--- src/interpreter/interpret/interpreter_stmt_test_.go
+++ src/interpreter/interpret/interpreter_stmt_test_.go
@@ -1,39 +1,186 @@
 package interpret
 
-import "testing"
+import (
+	. "Okra/src/interpreter/ast"
+	"testing"
+)
 
 func TestInterpretBlockStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretBlockStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretExpressionStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretExpressionStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretForStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretForStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretFuncStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretFuncStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretIfStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretIfStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretPrintStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretPrintStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretReturnStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretReturnStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretStructStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretStructStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
 
 func TestInterpretVariableStmt(t *testing.T) {
-	// TODO: Fill out tests!
+	table := []struct {
+		input  Stmt
+		output interface{}
+	}{
+		{},
+	}
+
+	for _, test := range table {
+		t.Run("TestInterpretVariableStmt", func(t *testing.T) {
+			interpreter := NewInterpreter()
+			stmt := interpreter.interpretStmt(test.input)
+
+			if stmt != test.output {
+				t.Errorf("Expected %v, recieved %v", test.output, stmt)
+			}
+		})
+	}
 }
